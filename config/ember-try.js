fa6fe457ed1a80c5d45f3ba074d94cc40e3e0a69
Marten Schilstra <mail@martenschilstra.nl>
2015-12-13 14:09:20 +0100

    Setup Travis.CI

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,24 +6,6 @@ module.exports = {
       dependencies: { }
     },
     {
-      name: 'ember-release',
-      dependencies: {
-        'ember': 'components/ember#release'
-      },
-      resolutions: {
-        'ember': 'release'
-      }
-    },
-    {
-      name: 'ember-beta',
-      dependencies: {
-        'ember': 'components/ember#beta'
-      },
-      resolutions: {
-        'ember': 'beta'
-      }
-    },
-    {
       name: 'ember-canary',
       dependencies: {
         'ember': 'components/ember#canary'
