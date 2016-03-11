9ef1b08958ac916120320504089aca01b5c54646
Marten Schilstra <mail@martenschilstra.nl>
2016-03-11 14:50:37 +0100

    Update to Ember CLI 2.4

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,26 +3,41 @@ module.exports = {
   scenarios: [
     {
       name: 'ember-2.3',
-      dependencies: {
-        'ember': '^2.3.0'
+      bower: {
+        dependencies: {
+          'ember': '~2.3.0'
+        }
+      }
+    },
+    {
+      name: 'ember-2.4',
+      bower: {
+        dependencies: {
+          'ember': '~2.4.0'
+        }
       }
     },
     {
       name: 'ember-beta',
-      dependencies: {
-        'ember': 'components/ember#beta'
-      },
-      resolutions: {
-        'ember': 'beta'
+
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#beta'
+        },
+        resolutions: {
+          'ember': 'beta'
+        }
       }
     },
     {
       name: 'ember-canary',
-      dependencies: {
-        'ember': 'components/ember#canary'
-      },
-      resolutions: {
-        'ember': 'canary'
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#canary'
+        },
+        resolutions: {
+          'ember': 'canary'
+        }
       }
     }
   ]
