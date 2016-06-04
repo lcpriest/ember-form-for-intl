0464cbb1aeca8610a3065093f2366c65a98aa12e
Marten Schilstra <mail@martenschilstra.nl>
2016-06-04 20:39:13 +0200

    Update demo with simple validation

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,13 +1,30 @@
 import Ember from 'ember';
 
+const { get, set, isEmpty } = Ember;
+
 export default Ember.Controller.extend({
   submit() {
     window.alert('Saved!');
   },
 
   reset() {
-    this.set('object', Ember.Object.create());
+    set(this, 'object', Ember.Object.create({ errors: {} }));
   },
 
-  object: Ember.Object.create()
+  update(object, propertyName, value) {
+    if (isEmpty(value)) {
+      let errors = get(object, `errors.${propertyName}`);
+
+      if (errors === undefined) {
+        errors = Ember.A();
+        set(object, `errors.${propertyName}`, errors);
+      }
+
+      errors.pushObject({ message: "can't be blank" });
+    }
+
+    set(object, propertyName, value);
+  },
+
+  object: Ember.Object.create({ errors: {} })
 });
