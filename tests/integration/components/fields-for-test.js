ea19c06bdc0ebb6b77fe573f0014e5fb1216fe12
Marten Schilstra <mail@martenschilstra.nl>
2016-07-27 20:20:13 +0200

    Favor fields-for over fieldset-for

--- tests/integration/components/fields-for-test.js
+++ tests/integration/components/fields-for-test.js
@@ -12,42 +12,36 @@ moduleForComponent('fieldset-for', 'Integration | Component | {{fieldset-for}}',
   }
 });
 
-test('It renders a fieldset element', function(assert) {
-  this.render(hbs`{{fieldset-for}}`);
-
-  assert.equal(this.$('fieldset').length, 1);
-});
-
 test('It yields an helper for rendering form components', function(assert) {
   this.render(hbs`
-    {{#fieldset-for object as |f|}}
+    {{#fields-for object as |f|}}
       {{f.text-field "name"}}
-    {{/fieldset-for}}
+    {{/fields-for}}
   `);
 
-  assert.equal(this.$('fieldset input[type="text"]').length, 1);
+  assert.equal(this.$('input[type="text"]').length, 1);
 });
 
 test('It puts the given attribute\'s value in the input', function(assert) {
   this.render(hbs`
-    {{#fieldset-for object as |f|}}
+    {{#fields-for object as |f|}}
       {{f.text-field "name"}}
-    {{/fieldset-for}}
+    {{/fields-for}}
   `);
 
-  let $input = this.$('fieldset input[type="text"]');
+  let $input = this.$('input[type="text"]');
 
   assert.equal($input.val(), 'Peter');
 });
 
 test('By default object properties are updated on typing', function(assert) {
   this.render(hbs`
-    {{#fieldset-for object as |f|}}
+    {{#fields-for object as |f|}}
       {{f.text-field "name"}}
-    {{/fieldset-for}}
+    {{/fields-for}}
   `);
 
-  let $input = this.$('fieldset input[type="text"]');
+  let $input = this.$('input[type="text"]');
 
   run(() => {
     $input.val('Robert');
