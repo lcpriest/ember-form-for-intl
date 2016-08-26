5828b59da0454492f6609c42ea748e3234fabccc
Marten Schilstra <mail@martenschilstra.nl>
2016-08-26 09:50:07 +0200

    Update Ember Try config

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,7 +1,57 @@
 /*jshint node:true*/
 module.exports = {
-  useVersionCompatibility: true,
   scenarios: [
+    {
+      name: 'ember-2.3',
+      bower: {
+        dependencies: {
+          "ember": "~2.3.0"
+        }
+      }
+    },
+    {
+      name: 'ember-lts',
+      bower: {
+        dependencies: {
+          "ember": "~2.4.0"
+        }
+      }
+    },
+    {
+      name: 'ember-latest',
+      bower: {
+        dependencies: {
+          "ember": "release"
+        },
+        resolutions: {
+          "ember": "release"
+        }
+      }
+    },
+    {
+      name: 'ember-beta',
+      allowedToFail: true,
+      bower: {
+        dependencies: {
+          "ember": "beta"
+        },
+        resolutions: {
+          "ember": "beta"
+        }
+      }
+    },
+    {
+      name: 'ember-canary',
+      allowedToFail: true,
+      bower: {
+        dependencies: {
+          "ember": "canary"
+        },
+        resolutions: {
+          "ember": "canary"
+        }
+      }
+    },
     {
       name: 'ember-alpha',
       allowedToFail: true,
