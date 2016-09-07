01178edcf75653dd51b2db1a4d00b53bef718ad3
Marten Schilstra <mail@martenschilstra.nl>
2016-09-07 16:32:37 +0200

    Add i18n support for checkbox-groups

---
+++ addon/components/form-fields/checkbox-group/option.js
@@ -0,0 +1,52 @@
+import Ember from 'ember';
+import layout from '../../../templates/components/form-fields/checkbox-group/option';
+
+import { humanize } from '../../../utils/strings';
+
+const {
+  String: { dasherize },
+  computed,
+  computed: { or },
+  get,
+  inject: { service },
+  isPresent
+} = Ember;
+
+export default Ember.Component.extend({
+  tagName: '',
+  layout,
+
+  i18n: service(),
+  config: service('ember-form-for/config'),
+
+  modelName: or('object.modelName', 'object.constructor.modelName'),
+
+  labelText: computed('value', 'label', 'labelI18nKey', function() {
+    let i18n = get(this, 'i18n');
+    let label = get(this, 'label');
+
+    if (isPresent(label)) {
+      return label;
+    } else if (isPresent(i18n)) {
+      return i18n.t(get(this, 'labelI18nKey'));
+    } else {
+      return get(this, 'label') || humanize(get(this, 'value'));
+    }
+  }),
+
+  labelI18nKey: computed('config.i18nKeyPrefix', 'modelName', 'propertyName', 'value', function() {
+    let value = get(this, 'value');
+
+    if (isPresent(value)) {
+      value = dasherize(value.toString());
+    }
+
+    return [
+      get(this, 'config.i18nKeyPrefix'),
+      dasherize(get(this, 'modelName') || ''),
+      dasherize(get(this, 'propertyName') || ''),
+      value
+    ].filter((x) => !!x)
+     .join('.');
+  })
+});
