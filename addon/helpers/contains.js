8025b203f9e6697683f48321dac5f4596d758a03
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2016-10-21 21:33:41 +0600

    Removes deprecation warning of using contains (#127)

--- addon/helpers/contains.js
+++ addon/helpers/contains.js
@@ -4,7 +4,7 @@ const { isArray } = Ember;
 
 export function contains([haystack, needle]) {
   if (isArray(haystack)) {
-    return Ember.A(haystack).contains(needle);
+    return Ember.A(haystack).includes(needle);
   } else {
     return haystack === needle;
   }
