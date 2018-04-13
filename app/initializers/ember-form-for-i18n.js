4ac84475af04a27b744ea02591de2dca21816515
Dan Patz <me@danielpatz.com>
2018-04-13 01:36:04 -0700

    Add support for Ember Engines (#193)

--- app/initializers/ember-form-for-i18n.js
+++ app/initializers/ember-form-for-i18n.js
@@ -1,5 +1,14 @@
+import Ember from 'ember';
+
 export function initialize(app) {
-  let i18n = app.__container__.lookup('service:i18n');
+  let i18n = null;
+
+  try {
+    i18n = Ember.getOwner(app).lookup('service:i18n');
+  } catch (e) {
+    i18n = app.__container__.lookup('service:i18n');
+  }
+
   if (i18n) {
     app.inject('component', 'i18n', 'service:i18n');
   }
